4f7d29319824b75d991055d0550bc39325db7f1f
Pavel Melnikov <positron96@gmail.com>
2013-01-09 22:21:40 +0700

    GetMaterialsForTypeWithActivity has 1 argument in crucible

--- src/eve-server/manufacturing/FactoryService.cpp
+++ src/eve-server/manufacturing/FactoryService.cpp
@@ -61,13 +61,14 @@ PyResult FactoryService::Handle_GetBlueprintAttributes(PyCallArgs &call) {
 }
 
 PyResult FactoryService::Handle_GetMaterialsForTypeWithActivity(PyCallArgs &call) {
-    Call_TwoIntegerArgs call_args;
+    //EVE crucible client sends only one arg with typeID
+    Call_SingleArg call_args;
     if(!call_args.Decode(&call.tuple)) {
         _log(SERVICE__MESSAGE, "Failed to decode args.");
         return NULL;
     }
 
-    return(m_db.GetMaterialsForTypeWithActivity(call_args.arg1));
+    return(m_db.GetMaterialsForTypeWithActivity(call_args.arg->AsInt()->value() ));
 }
 
 PyResult FactoryService::Handle_GetMaterialCompositionOfItemType(PyCallArgs &call) {
